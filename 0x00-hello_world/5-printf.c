a709f152bd1166c05bea7444a536d2f77914e128
samayo0507 <samuelayomide0705@gmail.com>
2022-09-08 13:45:09 -0700

    Print another statement

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@ nclude <stdio.h>
 
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
