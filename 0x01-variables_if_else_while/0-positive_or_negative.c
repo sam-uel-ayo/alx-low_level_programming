f190710450f9ca06e1d9c8cc7cd975dce1cd4f1a
samayo0507 <samuelayomide0705@gmail.com>
2022-09-09 07:24:29 -0700

    Assign random number to variable "n"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /**
- * main - print all possible combinations of single digits numbers
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int (main)void
 {
-		int number;
+	int n;
 
-		for (number = 0; number <= 9; number++)
-		{
-		putchar(number + '0');
-		if (number < 9)
-		{
-		putchar(',');
-		putchar(' ');
-		}
-		}
-		putchar('\n');
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-		return (0);
-}
+	if (n ==0)
+		printf("%d is zero\n", n;
+	else if (n < 0)
+		printf("d is negative\n", n);
+	else
+		printf("d is positive\n", n);
+
+	return (0);
+}	
