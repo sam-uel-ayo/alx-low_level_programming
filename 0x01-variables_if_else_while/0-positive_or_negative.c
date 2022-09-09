681c3226a39a6872828edd0a90f8d29f56576d4c
samayo0507 <samuelayomide0705@gmail.com>
2022-09-09 06:03:38 -0700

    Assign randon numbers to variable "n"

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,18 +8,18 @@
 
 int main(void)
 {
-	int number;
+		int number;
 
-	for (number = 0; number <9; number++)
-	{
-	putchar(number + '0');
-	if (number < 9)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
-	putchar('\n');
+		for (number = 0; number <9; number++)
+		{
+		putchar(number + '0');
+		if (number < 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
+		putchar('\n');
 
 	return (0);
 }
