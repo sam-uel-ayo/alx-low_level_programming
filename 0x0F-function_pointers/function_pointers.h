bb6c854469934f4bbb66e4d21c0c4ec36ce5a065
sam-uel-ayo <samuelayomide0705@gmail.com>
2022-10-12 23:26:38 +0100

    Header file for the project 0x0F-function_pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,8 @@
-#include "function_pointers.h"
-/**
- * print_name - is a function that prints a name
- * @name: pointer to name input
- * @f: pointer to void function
- * Return: void
- */
-void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-	{
-		f(name);
-	}
-}
+#ifndef function_pointers_h
+#define function_pointers_h
+#include <stddef.h>
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
